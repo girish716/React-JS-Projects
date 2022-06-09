d99f0566552d078db66f8b445b4e9e304a7becdc
girish716 <72153725+girish716@users.noreply.github.com>
2022-06-09 22:13:00 +0530

    Update Layout.jsx

--- ecommerce/components/Layout.jsx
+++ ecommerce/components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({children}) => {
   return (
     <div className='layout'>
       <Head>
-        <title>HeadPhones</title>
+        <title>Ecommerce</title>
       </Head>
       <header>
         <Navbar />
